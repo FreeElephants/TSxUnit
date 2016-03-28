35b49a357dea645b31bfe06b86328c7dd977e9f1
samizdam <samizdam@inbox.ru>
2016-03-28 22:56:35 +0300

    Move builder to sub ns

--- src/launch-builder.ts
+++ src/launch-builder.ts
@@ -1,8 +1,5 @@
 ///<reference path="../typings/main.d.ts"/>
-///<reference path="FreeElephants/TSxUnit/Scanner.ts"/>
-///<reference path="FreeElephants/TSxUnit/ScannerInterface.ts"/>
-///<reference path="FreeElephants/TSxUnit/Builder/LaunchMapBuilder.ts"/>
-///<reference path="FreeElephants/TSxUnit/LaunchMapInterface.ts"/>
+
 import MapBuilder = FreeElephants.TSxUnit.Builder.LaunchMapBuilder;
 var path = process.argv[2];
 var mapName = process.argv[3];
