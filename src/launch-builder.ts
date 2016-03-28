aa21d2d47c8f9499e7ce7c26e1bf8c1b9194d87c
samizdam <samizdam@inbox.ru>
2016-03-28 22:51:01 +0300

    rename TestCaseMapBuilder to LaunchMapBuilder

--- src/launch-builder.ts
+++ src/launch-builder.ts
@@ -1,7 +1,7 @@
 ///<reference path="../typings/main.d.ts"/>
 ///<reference path="../src/FreeElephants/TSxUnit/Scanner.ts"/>
 ///<reference path="../src/FreeElephants/TSxUnit/ScannerInterface.ts"/>
-///<reference path="../src/FreeElephants/TSxUnit/TestCaseMapBuilder.ts"/>
+///<reference path="FreeElephants/TSxUnit/LaunchMapBuilder.ts"/>
 ///<reference path="../src/FreeElephants/TSxUnit/LaunchMapInterface.ts"/>
 import TestCaseMapBuilder = FreeElephants.TSxUnit.TestCaseMapBuilder;
 var path = process.argv[2];
