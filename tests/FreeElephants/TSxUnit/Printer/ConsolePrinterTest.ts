0fe0cc551658fd84faaae1b849dc17f598bb2855
samizdam <samizdam@inbox.ru>
2016-04-11 22:41:21 +0300

    Add more method stubs to ConsoleprinterTests.

--- tests/FreeElephants/TSxUnit/Printer/ConsolePrinterTest.ts
+++ tests/FreeElephants/TSxUnit/Printer/ConsolePrinterTest.ts
@@ -1,18 +1,20 @@
-///<reference path="../../../../src/FreeElephants/TSxUnit/Test/ResultAdapterInterface.ts"/>
 namespace FreeElephants.TSxUnit.Printer {
 
     import Summary = FreeElephants.TSxUnit.Suite.Summary;
     import ResultList = FreeElephants.TSxUnit.Suite.ResultList;
     import TestCaseMethod = FreeElephants.TSxUnit.Test.TestCaseMethod;
-    import ResultAdapterInterface = FreeElephants.TSxUnit.Test.ResultAdapterInterface;
+    import AbstractResultAdapter = FreeElephants.TSxUnit.Test.AbstractResultAdapter;
 
     export class ConsolePrinterTest extends TestCase {
 
         public testSummaryWithFailed() {
             let printer = new ConsolePrinter();
-            let reason = this.getMockBuilder(ResultAdapterInterface).getMock();
+            let reason = this.getMockBuilder(AbstractResultAdapter)
+                .stubMethodWithValue("getMessage", "Failed asserting that foo is bar. ")
+                .stubMethodWithValue("getStack", "")
+                .getMock();
             let failuredTest = this.getMockBuilder(TestCaseMethod)
-                .stubMethodWithValue("getFullyQualifiedName", "Foo.testBar()")
+                .stubMethodWithValue("getFullyQualifiedName", "Foo.testBar")
                 .stubMethodWithValue("getSource", "tests/Foo.ts")
                 .stubMethodWithValue("getResult", reason)
                 .getMock();
@@ -26,15 +28,19 @@ namespace FreeElephants.TSxUnit.Printer {
             let buffer = printer.getBuffer();
 
             this.assertContains("1) Foo.testBar() [tests/Foo.ts]", buffer);
+            this.assertContains("Failed asserting that foo is bar. ", buffer);
             this.assertContains("FAILURES!", buffer);
             this.assertContains("Failures: 1", buffer);
         }
 
         public testSummaryWithError() {
             let printer = new ConsolePrinter();
-            let reason = this.getMockBuilder(ResultAdapterInterface).getMock();
+            let reason = this.getMockBuilder(AbstractResultAdapter)
+                .stubMethodWithValue("getMessage", "Error!11")
+                .stubMethodWithValue("getStack", "")
+                .getMock();
             let errorTest = this.getMockBuilder(TestCaseMethod)
-                .stubMethodWithValue("getFullyQualifiedName", "Foo.testBar()")
+                .stubMethodWithValue("getFullyQualifiedName", "Foo.testBar")
                 .stubMethodWithValue("getSource", "tests/Foo.ts")
                 .stubMethodWithValue("getResult", reason)
                 .getMock();
@@ -48,6 +54,7 @@ namespace FreeElephants.TSxUnit.Printer {
             let buffer = printer.getBuffer();
 
             this.assertContains("1) Foo.testBar() [tests/Foo.ts]", buffer);
+            this.assertContains("Error!11", buffer);
             this.assertContains("FAILURES!", buffer);
             this.assertContains("Errors: 1", buffer);
         }
